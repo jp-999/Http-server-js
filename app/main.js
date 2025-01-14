244dbffc09a86440893c09ddc70e22d9fbef8827
jp-999 <jayanthpeddi999@gmail.com>
2025-01-14 21:55:12 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -5,9 +5,13 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    // Respond with the HTTP 200 OK response
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.end(); // Close the socket after sending the response
+  });
   socket.on("close", () => {
     socket.end();
   });
 });
-
 server.listen(4221, "localhost");
