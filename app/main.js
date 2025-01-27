a7880e86d1b19d2e4ad39d81fb049770d24f34a6
jp-999 <jayanthpeddi999@gmail.com>
2025-01-27 23:03:46 +0530

    Stage4

--- app/main.js
+++ app/main.js
@@ -1,114 +1,218 @@
 const net = require("net");
+const fs = require("fs");
+const zlib = require("zlib");
 
-console.log("Logs from your program will appear here!");
+const PORT = 4221;
+const fileDir = process.argv[3];
 
-// Create a new HTTP server.
-const server = net.createServer((socket) => {
-    // This callback function is called when a new connection is established.
-    
-    // Listen for incoming data on the socket.
-    socket.on("data", (data) => {
-      // Convert the incoming data to a string.
-      const [requestLine, ...headers] = data.toString().split("\r\n");
-      
-      // Split the request line into the HTTP method and the requested path.
-      const [method, path] = requestLine.split(" ");
-      
-      // Handle different types of requests based on the path.
-      if (path === "/") {
-        // If the path is the root URL, send a 200 OK response.
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      } else if(path.startsWith("/echo/")) {
-        // If the path starts with '/echo/', handle the echo request.
-        handleEchoRequest(path, socket);
-      } else if(path === "/user-agent") {
-        // If the path is '/user-agent', handle the user agent request.
-        handleUserAgentRequest(headers, socket);
-      } else if(path.startsWith("/files/")) {
-        // If the path starts with '/files/', handle the file request.
-        handleFileRequest(method, path, headers, socket);
-      } else {
-        // If the path does not match any of the above, send a 404 Not Found response.
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      }
-      
-      // Close the socket after sending the response.
-      socket.end();
-    });
-    
-    // Listen for the 'close' event on the socket.
-    socket.on("close", () => {
-      // Close the socket when the 'close' event is emitted.
-      socket.end();
-    });
-  });
+const createHttpResponse = ({
+  message = "OK",
+  statusCode = 200,
+  body = "",
+  acceptEncoding = "",
+  contentType = "text/plain",
+  userAgent = "",
+  compressBody = false,
+}) => {
+  const HTTP_VERSION = "HTTP/1.1";
+  const NEW_LINE = "\r\n";
 
-server.listen(4221, "localhost");
+  let response_headers = {
+    headLine: `${HTTP_VERSION} ${statusCode} ${message}${NEW_LINE}`,
+    acceptEncoding: "Accept-Encoding: " + acceptEncoding + NEW_LINE,
+    contentType: "Content-Type: " + contentType + NEW_LINE,
+    contentLength: "Content-Length: " + body.length + NEW_LINE,
+  };
 
-// Function to handle echo requests.
-function handleEchoRequest(path, socket) {
-    // Extract the string to be echoed from the path.
-    const str = path.substring(6);
-    
-    // Send the echoed string as a response.
-    writeResponse(socket, "text/plain", str);
+  if (message !== "OK" && statusCode !== 200) {
+    response_headers.headLine = `${HTTP_VERSION} ${statusCode} ${message}${NEW_LINE}`;
   }
 
-// Function to handle user agent requests.
-function handleUserAgentRequest(headers, socket) {
-    // Find the 'User -Agent' header in the request headers.
-    const userAgentLine = headers.find(x => x.startsWith("User -Agent:"));
-    
-    // Extract the user agent string from the header.
-    const str = userAgentLine.split(": ")[1];
-    
-    // Send the user agent string as a response.
-    writeResponse(socket, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}", str);
+  if (acceptEncoding && acceptEncoding.includes("gzip")) {
+    response_headers.acceptEncoding = "Accept-Encoding: " + "gzip" + NEW_LINE;
+    response_headers.contentEncoding = "Content-Encoding: " + "gzip" + NEW_LINE;
   }
 
-// Function to handle file requests.
-function handleFileRequest(method, path, headers, socket) {
-    // Extract the filename from the path.
-    const filename = path.substring(7);
-    
-    // Handle GET requests.
-    if(method == "GET") {
-      // Check if the file exists.
-      if(fs.existsSync(paths.join(args[1], filename))) {
-        // Read the file content.
-        const fileContent = fs.readFileSync(paths.join(args[1], filename));
-        
-        // Send the file content as a response.
-        writeResponse(socket, "application/octet-stream", fileContent);
-      } else {
-        // Send a 404 Not Found response if the file does not exist.
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      }
-    } 
-    // Handle POST requests.
-    else if(method === "POST") {
-      // Get the location of the file to be written.
-      const location = paths.join(args[1], filename)
-      
-      // Write the file content.
-      fs.writeFileSync(location, headers[headers.length - 1]);
-      
-      // Send a 201 Created response.
-      socket.write("HTTP/1.1 201 Created\r\n\r\n");
+  if (contentType !== "text/plain") {
+    response_headers.contentType = "Content-Type: " + contentType + NEW_LINE;
+  }
+
+  if (userAgent !== "") {
+    response_headers.userAgent = "User-Agent: " + userAgent + NEW_LINE;
+  }
+
+  if (body !== "") {
+    if (
+      response_headers.contentEncoding?.includes("gzip") &&
+      compressBody === true
+    ) {
+      const compressed = zlib.gzipSync(body);
+
+      response_headers.contentLength =
+        "Content-Length: " + compressed.length + NEW_LINE; // overwrite content length
+
+      let response = "";
+
+      Object.values(response_headers).forEach((value) => {
+        response += value;
+      });
+
+      response += NEW_LINE; // every header adds a new line so after all headers add a new line so it becomes two new lines meaning end of headers
+
+      return [response, compressed];
     }
   }
 
-// Function to write a response to the socket.
-function writeResponse(socket, contentType, content) {
-    // Send the HTTP status line.
-    socket.write("HTTP/1.1 200 OK\r\n");
-    
-    // Send the 'Content-Type' header.
-    socket.write(`Content-Type: ${contentType}\r\n`);
+  let response = "";
+  Object.values(response_headers).forEach((value) => {
+    response += value;
+  });
 
-    // Send the 'Content-Length' header.
-socket.write(`Content-Length:${content.length}\r\n\r\n`);
-  
-// Send the response content.
-socket.write(content);
-}
+  response += NEW_LINE; // every header adds a new line so after all headers add a new line so it becomes two new lines meaning end of headers
+
+  response += body;
+
+  return response;
+};
+
+const server = net.createServer((socket) => {
+  socket.on("close", () => {
+    socket.end();
+  });
+
+  socket.on("data", (data) => {
+    const stringData = data.toString();
+    console.log(stringData);
+
+    const arrayData = stringData.split("\r\n");
+
+    const body = arrayData[arrayData.length - 1];
+
+    const methodPathVersion = arrayData[0].split(" ");
+    const method = methodPathVersion[0];
+    const path = methodPathVersion[1];
+    const version = methodPathVersion[2];
+
+    const request = {
+      method: method,
+      path: path,
+      version: version,
+    };
+
+    if (request.path === "/") {
+      const response = createHttpResponse({});
+      socket.write(response);
+    }
+
+    arrayData.map((header) => {
+      if (header.includes("User-Agent:")) {
+        let splited = header.split(": ");
+        request.userAgent = splited[1];
+      }
+      if (header.includes("Accept-Encoding:")) {
+        let splited = header.split(": ");
+        request.acceptEncoding = splited[1];
+      }
+    });
+
+    if (request.path === "/user-agent") {
+      const ACCEPTED_ENCODING = "gzip";
+      const userAgent = request.userAgent;
+      const acceptEncoding = request.acceptEncoding;
+
+      if (!userAgent) {
+        const response = createHttpResponse({
+          message: "Bad Request",
+          statusCode: 400,
+        });
+        socket.write(response);
+        socket.end();
+        return;
+      }
+
+      if (acceptEncoding?.includes(ACCEPTED_ENCODING)) {
+        const response = createHttpResponse({
+          message: "OK",
+          statusCode: 200,
+          acceptEncoding: "gzip",
+          body: userAgent,
+        });
+        socket.write(response);
+      } else {
+        const response = createHttpResponse({
+          message: "OK",
+          statusCode: 200,
+          acceptEncoding: "gzip",
+          body: userAgent,
+        });
+        socket.write(response);
+        socket.end();
+      }
+    }
+
+    const ECHO_PART_LENGTH = 6; // "/echo/"
+    request.echoPart = request.path.slice(0, ECHO_PART_LENGTH);
+    request.restPart = request.path.slice(ECHO_PART_LENGTH);
+
+    if (request.echoPart === "/echo/") {
+      let response;
+      let compressedBody;
+      if (request.acceptEncoding?.includes("gzip")) {
+        [response, compressedBody] = createHttpResponse({
+          message: "OK",
+          statusCode: 200,
+          body: request.restPart,
+          acceptEncoding: request.acceptEncoding,
+          compressBody: true,
+        });
+      } else {
+        response = createHttpResponse({
+          message: "OK",
+          statusCode: 200,
+          body: request.restPart,
+          acceptEncoding: request.acceptEncoding,
+        });
+      }
+      socket.write(response);
+      if (compressedBody) {
+        socket.write(compressedBody);
+        socket.end();
+      }
+    } else if (request.method === "GET" && request.path.startsWith("/files")) {
+      const fileName = request.path.split("/")[2];
+      const filePath = `${fileDir}/${fileName}`;
+
+      if (!fs.existsSync(filePath)) {
+        const response = createHttpResponse({
+          message: "Not Found",
+          statusCode: 404,
+        });
+        socket.write(response);
+        socket.end();
+      } else {
+        const fileContent = fs.readFileSync(filePath);
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Encoding: ${request.acceptEncoding}\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}\r\n`
+        );
+        socket.end();
+      }
+    } else if (request.method === "POST" && request.path.startsWith("/files")) {
+      const fileName = request.path.split("/")[2];
+      const filePath = `${fileDir}/${fileName}`;
+      fs.writeFileSync(filePath, body);
+      const response = createHttpResponse({
+        message: "Created",
+        statusCode: 201,
+      });
+      socket.write(response);
+    } else {
+      const response = createHttpResponse({
+        message: "Not Found",
+        statusCode: 404,
+      });
+      socket.write(response);
+    }
+    socket.end();
+  });
+});
+server.listen(PORT, "localhost");
